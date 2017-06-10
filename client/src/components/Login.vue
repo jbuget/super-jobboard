cc041ab8456d91b15cddd25a97fa6a1922b3cf96
Jérémy Buget <jbuget@gmail.com>
2017-06-10 02:38:55 +0200

    Import Google Sign-in script and meta into index.html (client)

---
+++ client/src/components/Login.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="login">
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'login'
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
